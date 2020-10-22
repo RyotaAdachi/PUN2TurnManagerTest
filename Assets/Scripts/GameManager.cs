868895019498def12f64462542b50ca7c74ae70a
Ryota Adachi <ryota@RyotanoMacBook-Air.local>
2020-10-22 20:32:40 +0900

    add PUN2 package

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using Photon.Pun.UtilityScripts;
+
+public class GameManager : MonoBehaviourPunCallbacks, IPunTurnManagerCallbacks
+{
+
+    [SerializeField] PunTurnManager turnManager;
+    [SerializeField] float time;
+
+    void Awake()
+    {
+        this.turnManager.TurnManagerListener = this;
+    }
+
+    void Start()
+    {
+        StartTurn();
+    }
+
+    void Update()
+    {
+        time = turnManager.RemainingSecondsInTurn;
+    }
+
+    public void OnTurnBegins(int turn)
+    {
+
+    }
+    public void OnTurnCompleted(int turn)
+    {
+
+    }
+    public void OnPlayerMove(Player player, int turn, object move)
+    {
+
+    }
+    public void OnPlayerFinished(Player player, int turn, object move)
+    {
+
+    }
+    public void OnTurnTimeEnds(int turn)
+    {
+
+    }
+
+    public void StartTurn()//ターン開始メソッド（シーン開始時にRPCから呼ばれる呼ばれるようにしてあります。）
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            this.turnManager.BeginTurn();//turnmanagerに新しいターンを始めさせる
+        }
+    }
+}
---
+++ Assets/Scripts/QuickMatch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class QuickMatch : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.NetworkClientState.ToString());
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinOrCreateRoom("room", new RoomOptions(), TypedLobby.Default);
+    }
+}
