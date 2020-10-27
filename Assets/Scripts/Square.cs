c580df07b7a439c608d4818ceef2086be2c3b58d
Ryota Adachi <ryota@RyotanoMacBook-Air.local>
2020-10-28 00:01:21 +0900

    マスをクリックしてマークできるようにした

---
+++ Assets/Scripts/Square.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class Square : MonoBehaviour
+{
+    public enum Marks
+    {
+        None,
+        Circle,
+        Cross
+    }
+
+    // マスの状態
+    [SerializeField] Marks mark;
+    [SerializeField] Image markImage;
+    [SerializeField] Sprite noneIcon;
+    [SerializeField] Sprite circleIcon;
+    [SerializeField] Sprite crossIcon;
+
+
+    // コールバック
+    public UnityAction<Square> ClickAction;
+
+    public void Init(int id)
+    {
+        mark = Marks.None;
+        Show();
+    }
+
+    void Show()
+    {
+        if (mark == Marks.Circle)
+        {
+            this.markImage.sprite = circleIcon;
+        }
+        else if (mark == Marks.Cross)
+        {
+            this.markImage.sprite = crossIcon;
+        }
+        else
+        {
+            this.markImage.sprite = noneIcon;
+        }
+    }
+
+    public Marks GetMark()
+    {
+        return mark;
+    }
+
+    public void ChangeMark(Marks mark)
+    {
+        this.mark = mark;
+        Show();
+    }
+
+    public void OnClickThis()
+    {
+        Debug.Log("OnClicked");
+        ClickAction.Invoke(this);
+    }
+
+}
---
+++ Assets/Scripts/TicTocToe.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TicTocToe : MonoBehaviour
+{
+    [SerializeField] Square squarePrefab;
+    [SerializeField] Transform panel;
+    List<Square> squares = new List<Square>();
+    List<Square[]> lines = new List<Square[]>();
+
+    void Start()
+    {
+        GenerateBoard();
+    }
+
+    void GenerateBoard()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            Square square = Instantiate(squarePrefab, Vector3.zero, Quaternion.identity, panel);
+            square.ClickAction = SelectSquare;
+            squares.Add(square);
+        }
+
+        lines.Add(new Square[] {squares[0], squares[1], squares[2]});
+        lines.Add(new Square[] {squares[3], squares[4], squares[5]});
+        lines.Add(new Square[] {squares[6], squares[7], squares[8]});
+        lines.Add(new Square[] {squares[0], squares[3], squares[6]});
+        lines.Add(new Square[] {squares[1], squares[4], squares[7]});
+        lines.Add(new Square[] {squares[2], squares[5], squares[8]});
+        lines.Add(new Square[] {squares[0], squares[4], squares[8]});
+        lines.Add(new Square[] {squares[2], squares[4], squares[6]});
+    }
+
+    void SelectSquare(Square square)
+    {
+        Debug.Log("SelectSquare");
+        square.ChangeMark(Square.Marks.Circle);
+
+        if(IsGameEnd())
+        {
+            // Game End
+        }
+        
+    }
+
+    bool IsGameEnd()
+    {
+        foreach(Square[] s in lines)
+        {
+            if(s[0].GetMark() != Square.Marks.None)
+            {
+                if(s[0].GetMark() == s[1].GetMark() && s[0].GetMark() == s[2].GetMark())
+                {
+                    Debug.Log("Game End!");
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+
+}
